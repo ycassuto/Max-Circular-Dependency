3c6f57cd1bf8f4c4085f4daf317406924e311aaa
cassuti <yuvc210@gmail.com>
2022-10-25 17:04:13 +0300

    add solutions

---
+++ Interface_solution/Interface_solution/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_solution
+{
+    interface ICall
+    { 
+        void CallEmployees();
+    }
+
+    class A : ICall
+    {
+        public void C()
+        {
+            B b = new B();
+            b.MakeCoffee(this);
+        }
+
+        public void CallEmployees()
+        {
+            Console.WriteLine("call the emploies");
+        }
+    }
+
+    class B
+    {
+        public void MakeCoffee(ICall caller)
+        {
+            //task 1
+            //task 2
+            Console.WriteLine("half way there");
+            caller.CallEmployees();
+            //task 3
+            //task 4
+            Console.WriteLine("finish coffee make");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new A();
+            a.C();
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ event solution/event solution/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace event_solution
+{
+    class A 
+    {
+        public A()
+        {
+            SubscribeToEvent();
+        }
+
+        public void C()
+        {
+            B b = new B();
+            b.MakeCoffee();
+        }
+
+        public void CallEmployees()
+        {
+            Console.WriteLine("call the emploies");
+        }
+
+        private void SubscribeToEvent()
+        {
+            LettersEvents.onCoffeeHalfDone += CallEmployees;
+        }
+    }
+
+    class B
+    {
+        public void MakeCoffee()
+        {
+            //task 1
+            //task 2
+            Console.WriteLine("half way there");
+            LettersEvents.CoffeeHalfDone();
+            //task 3
+            //task 4
+            Console.WriteLine("finish coffee make");
+        }
+    }
+
+    public static class LettersEvents
+    {
+        public static event Action onCoffeeHalfDone;
+        public static void CoffeeHalfDone()
+        {
+            if (onCoffeeHalfDone != null)
+            {
+                onCoffeeHalfDone();
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new A();
+            a.C();
+            Console.ReadLine();
+        }
+    }
+}
